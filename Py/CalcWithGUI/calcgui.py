c08fa8ba855dbbc55f0d5a5d0e67c0dcaf4116d7
Saketh <saketh3.14159@gmail.com>
2025-06-10 19:25:47 +0530

    testing github

---
+++ Py/CalcWithGUI/calcgui.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evalute_calculation():
+    pass
+
+def clear_calculation():
+    pass
+
+
+root = tk.Tk() # Create the main window
+root.geometry("300x275") #Set the size of the window
+
+text_result = 
+root.mainloop() # Start the main event loop, basically the program runs until the window is closed.
---
+++ Py/CalcWithGUI/test.py
@@ -0,0 +1,6 @@
+print("Testing git for the first time meow meow")
+
+def test_function():
+    print("MEOW")
+
+test_function()
