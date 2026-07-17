1ea64e9704189ceec1d2dd0cf06c1b77fa64fb11
A <122394189+g0so@users.noreply.github.com>
2026-07-17 19:59:31 +0530

    Delete test_leetcode.py

--- test_leetcode.py
+++
@@ -1,8 +0,0 @@
-# test_leetcode.py
-# Temporary file just to test leetcode_api.py works correctly.
-# You can delete this after confirming it works.
-
-from leetcode_api import get_daily_problem
-
-problem = get_daily_problem()
-print(problem)
